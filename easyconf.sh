c9329df5cff6870993f565a1b024a3012c1515f2
samuelgutierrez <samuelgutierrez@979fb63a-54b4-4514-8d67-9ab8ea1b59df>
2010-07-27 20:05:00 +0000

    added a README, bye bye README.hacking, cleanup
    git-svn-id: https://plfs.svn.sourceforge.net/svnroot/plfs/trunk@221 979fb63a-54b4-4514-8d67-9ab8ea1b59df

--- easyconf.sh
+++ easyconf.sh
@@ -5,7 +5,7 @@
 SCRIPT_PATH="${BASH_SOURCE[0]}";
 
 if ([ -h "${SCRIPT_PATH}" ]) then
-    while([ -h "${SCRIPT_PATH}" ]); do
+    while ([ -h "${SCRIPT_PATH}" ]); do
         SCRIPT_PATH=`readlink "${SCRIPT_PATH}"`
     done
 fi
