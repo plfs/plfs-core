0a7966ca3597b89cefecfd9966c5686136173035
samuelgutierrez <samuelgutierrez@979fb63a-54b4-4514-8d67-9ab8ea1b59df>
2010-07-27 19:32:58 +0000

    fix another alt build problem, easyconf from another path
    git-svn-id: https://plfs.svn.sourceforge.net/svnroot/plfs/trunk@220 979fb63a-54b4-4514-8d67-9ab8ea1b59df

--- easyconf.sh
+++ easyconf.sh
@@ -2,7 +2,19 @@
 
 # for a full list of what options are available, try ./configure --help
 
-conf_cmd_str="./configure"
+SCRIPT_PATH="${BASH_SOURCE[0]}";
+
+if ([ -h "${SCRIPT_PATH}" ]) then
+    while([ -h "${SCRIPT_PATH}" ]); do
+        SCRIPT_PATH=`readlink "${SCRIPT_PATH}"`
+    done
+fi
+pushd . > /dev/null
+cd `dirname ${SCRIPT_PATH}` > /dev/null
+SCRIPT_PATH=`pwd`;
+popd  > /dev/null
+
+conf_cmd_str="$SCRIPT_PATH/configure"
 
 # disable silent rules
 if [[ "$PLFS_WANT_VERBOSE_CONF_OUT" != "" && "$PLFS_WANT_VERBOSE_CONF_OUT" != "0" ]]; then
