deb393884ed12569b3920ae3f65376fdece823ba
johnbent <johnbent@979fb63a-54b4-4514-8d67-9ab8ea1b59df>
2010-09-23 04:47:49 +0000

    Ok, made the truncate test pretty sweet.  Does overwriting in the middle as well.  PLFS checks out!
    git-svn-id: https://plfs.svn.sourceforge.net/svnroot/plfs/trunk@313 979fb63a-54b4-4514-8d67-9ab8ea1b59df

--- tests/truncate.py
+++ tests/truncate.py
@@ -17,14 +17,16 @@ def getDefaultTarget():
   return "foo"
 
 parser = optparse.OptionParser()
-parser.add_option("--truncate_len", help="The length to truncate the file", 
-  default=None)
+parser.add_option("--size", "-s", help="The total length of the file", 
+  default=1048576, type="int")
 parser.add_option("--truncate_only", action="store_true", 
   help="Skip the initial create [default=%default]", default=False)
 parser.add_option("-t", dest="target", 
   help="target file [default=%default]", default=getDefaultTarget())
 parser.add_option("-p", dest="preserve", action="store_true",
   help="don't unlink file after tests [default=%default]", default=False)
+parser.add_option("-v", dest="verbose", action="store_true",
+  help="verbose data checking if error [default=%default]", default=False)
 
 def openFile(path,mode):
   try:
@@ -34,7 +36,7 @@ def openFile(path,mode):
     exit(-1)
   return f
 
-def checkData(path,data,phase):
+def checkData(path,data,phase,verbose):
   fdata = None 
   f = openFile(path,'r')
   fdata = f.read()
@@ -44,37 +46,63 @@ def checkData(path,data,phase):
     print 'Data integrity error after %s' % phase
     if(len(fdata)!=len(data)):
       print "Length mismatch %d != %d" %(len(fdata),len(data))
-    print 'Expected %s, got %s' % (fdata,data)
-    for i in range(0,len(fdata)):
-      print "%c ? %c" % (data[i],fdata[i])
+    print 'Expected %s, got %s' % (data,fdata)
+    if (verbose is True):
+      for i in range(0,min(len(fdata),len(data))):
+        print "%c ? %c" % (data[i],fdata[i])
     exit(-2)
 
-def main():
-  print "I made it into the the main"
-  (options, args) = parser.parse_args()
-  data="hello world"
-  if ( options.truncate_only is not None ):
-    f = openFile(options.target,'w')
-    print "Writing '%s' to %s" % (data,options.target)
-    f.write(data)
-    f.close()
-
-  checkData(options.target,data,phase='write') 
-
-  if ( options.truncate_len is not None ):
-    tlen = options.truncate_len
-  else:
-    tlen = int(len(data)/2)
-  f = openFile(options.target, 'a')
+def truncateFile(target,tlen):
+  f = openFile(target, 'a')
   try:
     f.truncate(tlen)
   except IOError: 
       print "Truncate error"
-  print "Truncated '%s' to %d" % (data,tlen)
+  print "Truncated %s to %d" % (target,tlen)
   f.close()
 
-  checkData(options.target,data[0:tlen],'truncate') 
+def writeFile(target,data,offset,mode):
+  fd = os.open(target, mode)
+  f = os.fdopen(fd,'w')
+  f.seek(offset,0)
+  print "Writing %d bytes to %s at off %d" % (len(data),target,offset)
+  f.write(data)
+  f.close()
 
+def main():
+  (options, args) = parser.parse_args()
+  data="a"*options.size
+
+  # create the file
+  if ( options.truncate_only is not None ):
+    writeFile(options.target,data,0,os.O_WRONLY|os.O_CREAT|os.O_TRUNC)
+    checkData(options.target,data,phase='write',verbose=options.verbose) 
+
+  # truncate it
+  tlen = int(options.size/2)
+  truncateFile(options.target,tlen)
+  data=data[:tlen]
+  checkData(options.target,data,phase='truncate',verbose=options.verbose) 
+
+  # overwrite a tenth of it
+  data2="b"*int(options.size/10)
+  offset=int(options.size/5)
+  expected  = data[:offset]
+  expected += data2
+  expected += data[offset+len(data2):len(data)]
+  writeFile(options.target,data2,offset,os.O_WRONLY)
+  checkData(options.target,expected,phase='overwrite',verbose=options.verbose)
+
+  # truncate it one more time in the middle of the overwritten piece
+  tlen = offset + int(len(data2)/2)
+  expected=expected[:tlen]
+  truncateFile(options.target,tlen)
+  checkData(options.target,expected,phase='truncate2',verbose=options.verbose) 
+
+
+  print "No errors!"
+
+  # clean up
   if ( options.preserve is False ):
     os.unlink(options.target)
 
