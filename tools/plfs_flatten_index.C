dc90e409d0fcd8fa7b384868d61653e763452287
johnbent <johnbent@979fb63a-54b4-4514-8d67-9ab8ea1b59df>
2010-10-16 05:16:16 +0000

    git-svn-id: https://plfs.svn.sourceforge.net/svnroot/plfs/trunk@374 979fb63a-54b4-4514-8d67-9ab8ea1b59df

--- tools/plfs_flatten_index.C
+++ tools/plfs_flatten_index.C
@@ -1,28 +1,18 @@
 #include <string.h>
 #include <stdlib.h>
+#include <errno.h>
+
 #include "plfs.h"
-#include "Container.h"
 #include "COPYRIGHT.h"
-#include "Util.h"
 
 int main (int argc, char **argv) {
     const char *target  = argv[1];
-	/*
-    if ( ! is_plfs_file( target, NULL ) ) {
-        fprintf( stderr, "%s is not a container\n", target );
-        exit( 0 );
-    }
-	*/
-    Index index( target, 0 );
-    int ret = Container::populateIndex( target, &index );
+    int ret = plfs_flatten_index(NULL,target);
     if ( ret != 0 ) {
         fprintf( stderr, "Couldn't read index from %s: %s\n", 
-                target, strerror(errno) );
+                target, strerror(-ret));
+    } else {
+        printf("Successfully flattened index of %s\n",target);
     }
-        // can't compress if you want to do the viz
-        // this needs to be command line flag clearly
-    index.compress();
-    cerr << "# INDEX DUMP OF " << target << endl;
-    cout << index << endl;
-    exit( 0 );
+    exit( ret );
 }
--- tools/plfs_map.C
+++ tools/plfs_map.C
@@ -2,27 +2,14 @@
 #include <stdlib.h>
 #include "plfs.h"
 #include "Container.h"
-#include "COPYRIGHT.h"
-#include "Util.h"
 
 int main (int argc, char **argv) {
     const char *target  = argv[1];
-	/*
-    if ( ! is_plfs_file( target, NULL ) ) {
-        fprintf( stderr, "%s is not a container\n", target );
-        exit( 0 );
-    }
-	*/
-    Index index( target, 0 );
-    int ret = Container::populateIndex( target, &index );
+    bool compress = true;
+    int ret = plfs_dump_index(stderr,target,compress);
     if ( ret != 0 ) {
-        fprintf( stderr, "Couldn't read index from %s: %s\n", 
-                target, strerror(errno) );
+        fprintf(stderr, "Couldn't read index from %s: %s\n",
+                target, strerror(-ret));
     }
-        // can't compress if you want to do the viz
-        // this needs to be command line flag clearly
-    index.compress();
-    cerr << "# INDEX DUMP OF " << target << endl;
-    cout << index << endl;
-    exit( 0 );
+    exit( ret );
 }
