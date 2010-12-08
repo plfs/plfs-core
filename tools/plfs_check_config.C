ae3877f46f7c7b16b3354acf3619ce7187f4491f
johnbent <johnbent@979fb63a-54b4-4514-8d67-9ab8ea1b59df>
2010-12-08 18:40:06 +0000

    git-svn-id: https://plfs.svn.sourceforge.net/svnroot/plfs/trunk@419 979fb63a-54b4-4514-8d67-9ab8ea1b59df

---
+++ tools/plfs_check_config.C
@@ -0,0 +1,11 @@
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+
+#include "plfs.h"
+#include "COPYRIGHT.h"
+
+int main (int argc, char **argv) {
+    int ret = 0; //plfs_dump_config();
+    exit( ret );
+}
