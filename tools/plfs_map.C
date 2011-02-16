d439f6a2c884adcb19e114b3f4aa1802013e3972
lagos <lagos@979fb63a-54b4-4514-8d67-9ab8ea1b59df>
2011-02-16 20:02:49 +0000

    Added some more logic to plfs_map.C to create a -nc flag to disable compression. This is for use for map distribution analysis.
    git-svn-id: https://plfs.svn.sourceforge.net/svnroot/plfs/trunk@481 979fb63a-54b4-4514-8d67-9ab8ea1b59df

--- tools/plfs_map.C
+++ tools/plfs_map.C
@@ -3,9 +3,31 @@
 #include "plfs.h"
 #include "Container.h"
 
+void show_usage(char* app_name) {
+	fprintf(stderr, "Usage: %s [-nc] <filename>\n", app_name);
+}
+
 int main (int argc, char **argv) {
-    const char *target  = argv[1];
-    bool compress = true;
+	int i;
+	bool compress = true;
+	char *target;
+	bool found_target = false;
+	for (i = 1; i < argc; i++) {
+		if (strcmp(argv[i], "-nc") == 0) {
+			compress = false;
+		} else if (!found_target) {
+			target = argv[i];
+		} else {
+			// Found more than one target. This is an error.
+			show_usage(argv[0]);
+			exit(1);
+		}
+	}
+	if (!found_target) {
+		show_usage(argv[0]);
+		exit(1);
+	}
+		
     int ret = plfs_dump_index(stderr,target,(int)compress);
     if ( ret != 0 ) {
         fprintf(stderr, "Couldn't read index from %s: %s\n",
