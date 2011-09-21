f065504b66e5ea7db6baecddcb6d03f0356224af
johnbent <johnbent@979fb63a-54b4-4514-8d67-9ab8ea1b59df>
2011-09-21 02:28:42 +0000

    the Util::tokenize function was instead a #ifdef statement where it shouldn't have been
    git-svn-id: https://plfs.svn.sourceforge.net/svnroot/plfs/trunk@653 979fb63a-54b4-4514-8d67-9ab8ea1b59df

--- src/Util.cpp
+++ src/Util.cpp
@@ -118,30 +118,6 @@ Util::Debug( const char *format, ... ) {
     va_end( args );
 }
 
-// function that tokenizes a string into a set of strings based on set of delims
-vector<string> &Util::tokenize(const string& str,const string& delimiters,
-        vector<string> &tokens)
-{
-	// skip delimiters at beginning.
-    string::size_type lastPos = str.find_first_not_of(delimiters, 0);
-    	
-	// find first "non-delimiter".
-    string::size_type pos = str.find_first_of(delimiters, lastPos);
-
-    while (string::npos != pos || string::npos != lastPos) {
-        // found a token, add it to the vector.
-        tokens.push_back(str.substr(lastPos, pos - lastPos));
-		
-        // skip delimiters.  Note the "not_of"
-        lastPos = str.find_first_not_of(delimiters, pos);
-		
-        // find next "non-delimiter"
-        pos = str.find_first_of(delimiters, lastPos);
-    }
-
-	return tokens;
-}
-
 void
 Util::Debug( const char *format, va_list args ) {
     static FILE *debugfile = NULL;
@@ -170,6 +146,30 @@ Util::Debug( const char *format, va_list args ) {
 void Util::Debug( const char *format, ... ) { return; }
 void Util::Debug( const char *format, va_list args ) { return; }
 #endif
+
+// function that tokenizes a string into a set of strings based on set of delims
+vector<string> &Util::tokenize(const string& str,const string& delimiters,
+        vector<string> &tokens)
+{
+	// skip delimiters at beginning.
+    string::size_type lastPos = str.find_first_not_of(delimiters, 0);
+    	
+	// find first "non-delimiter".
+    string::size_type pos = str.find_first_of(delimiters, lastPos);
+
+    while (string::npos != pos || string::npos != lastPos) {
+        // found a token, add it to the vector.
+        tokens.push_back(str.substr(lastPos, pos - lastPos));
+		
+        // skip delimiters.  Note the "not_of"
+        lastPos = str.find_first_not_of(delimiters, pos);
+		
+        // find next "non-delimiter"
+        pos = str.find_first_of(delimiters, lastPos);
+    }
+
+	return tokens;
+}
 
 void
 Util::SeriousError( string msg, pid_t pid ) {
