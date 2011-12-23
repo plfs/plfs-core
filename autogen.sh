e206a998c26419f6d802c80e6b6461485fbdc961
samuelgutierrez <samuelgutierrez@979fb63a-54b4-4514-8d67-9ab8ea1b59df>
2011-12-23 04:15:17 +0000

    fix something about this script that has annoyed me for a long time. exact tool versions are no longer required.  now only minimum requirements need to be met.  also cleanup some other random things... enjoy!
    git-svn-id: https://plfs.svn.sourceforge.net/svnroot/plfs/trunk@770 979fb63a-54b4-4514-8d67-9ab8ea1b59df

--- autogen.sh
+++ autogen.sh
@@ -1,49 +1,81 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# bypass version checks :-(
+# flag that indicates whether or not version checks will be bypassed
+# default: disabled
 force=0
 
-m4_req_version="1.4.14"
-ac_req_version="2.65"
-am_req_version="1.11.1"
-lt_req_version="2.2.6b"
+# MINIMUM version requirements
+# gnu m4
+m4_min_version_req="1.4.14"
+# gnu autoconf
+ac_min_version_req="2.65"
+# gnu automake
+am_min_version_reg="1.11.1"
+# gnu libtool
+lt_min_version_req="2.2.6b"
 
-req_versions=($m4_req_version $ac_req_version $am_req_version $lt_req_version)
+min_requirements=(  \
+$m4_min_version_req \
+$ac_min_version_req \
+$am_min_version_reg \
+$lt_min_version_req \
+)
+
+# list of tools that will be checked
 tools=(m4 autoconf automake libtool)
 
+################################################################################
+# prints tool usage
+# args: none
+################################################################################
 autogen_usage()
 {
-    echo "usage: autogen.sh [-f | --force]"
+    echo "usage: autogen.sh [-h | --help] [-f | --force]"
 }
 
+################################################################################
+# prints tool prerequisites and requirements
+# args: none
+################################################################################
 echo_tool_reqs()
 {
 cat << EOF
 
 autogen prerequisites
 *********************
-m4: $m4_req_version
-autoconf: $ac_req_version
-automake: $am_req_version
-libtool: $lt_req_version
+m4: $m4_min_version_req
+autoconf: $ac_min_version_req
+automake: $am_min_version_reg
+libtool: $lt_min_version_req
 
 EOF
 }
 
+################################################################################
+# prints tool prerequisites 
+# args:
+# $1: tool name
+# $2: minimum tool version
+################################################################################
 check_tool_version()
 {
     v_output=`$1 --version`
-    if [[ $? != 0 ]]
-    then
-        echo "!!! could not determine $1's version !!!"
+    if [[ $? != 0 ]]; then
+        echo "!!! could not determine $1's version. cannot continue !!!"
         echo_tool_reqs
         exit 1
     fi
     version_str=`echo -e "$v_output" | grep -i $1`
     version_str=`echo -e "$version_str" | \
     grep -oe '[0-9]\+[.][0-9]\+[.]\?[0-9]*[A-Za-z]\?'`
-    if [[ $2 != $version_str ]]
-    then
+
+    # at this point we should have a version number. something like: 1.4.16
+    
+    # save the newest version
+    newest_v=`echo "$version_str $2" | tr ' ' '\n' | sort -nr | head -n 1`
+
+    # does this tool meet our minimum requirements?
+    if [[ $version_str != $newest_v ]]; then
         echo "$1 version requirement not met - detected $1 $version_str"
         echo_tool_reqs
         exit 1
@@ -55,10 +87,10 @@ do
     case "$1" in
         -h)
             autogen_usage
-            exit;;
+            exit 0;;
        --help)
             autogen_usage
-            exit;;
+            exit 0;;
        -f)
             force=1
             shift;;
@@ -67,31 +99,31 @@ do
             shift;;
        *)
             autogen_usage
-            exit;;
+            exit 1;;
     esac
     shift
 done
 
 echo "thinking ..."
 
-if [[ $force == 1 ]]
-then
+if [[ $force == 1 ]]; then
     # you are on your own :-)
     echo "##############################################"
     echo "WARNING ***BYPASSING VERSION CHECKS*** WARNING"
     echo "##############################################"
 else
-    for i in ${!tools[*]}
-    do
-        check_tool_version ${tools[$i]} ${req_versions[$i]}
+    for i in ${!tools[*]}; do
+        check_tool_version ${tools[$i]} ${min_requirements[$i]}
     done
 fi
 
 # if we are here, all is good. let the real work begin...
 autoreconf --force --install -I config
 
-# Copyright (c) 2009-2010, Los Alamos National Security, LLC. All rights
-# reserved.
+exit $?
+
+# Copyright (c) 2009-2011, Los Alamos National Security, LLC.
+#                          All rights reserved.
 #
 # This software was produced under U.S. Government contract DE-AC52-06NA25396
 # for Los Alamos National Laboratory (LANL), which is operated by Los Alamos
@@ -104,8 +136,8 @@ autoreconf --force --install -I config
 # from LANL.
 #
 # Additionally, redistribution and use in source and binary forms, with or
-# without modification, are permitted provided that the following conditions are
-# met:
+# without modification, are permitted provided that the following conditions
+# are met:
 #
 # •    Redistributions of source code must retain the above copyright notice,
 # this list of conditions and the following disclaimer.
@@ -114,19 +146,19 @@ autoreconf --force --install -I config
 # this list of conditions and the following disclaimer in the documentation
 # and/or other materials provided with the distribution.
 #
-# •   Neither the name of Los Alamos National Security, LLC, Los Alamos National
-# Laboratory, LANL, the U.S. Government, nor the names of its contributors may
-# be used to endorse or promote products derived from this software without
-# specific prior written permission.
+# •   Neither the name of Los Alamos National Security, LLC, Los Alamos
+# National Laboratory, LANL, the U.S. Government, nor the names of its
+# contributors may be used to endorse or promote products derived from this
+# software without specific prior written permission.
 #
 # THIS SOFTWARE IS PROVIDED BY LOS ALAMOS NATIONAL SECURITY, LLC AND
-# CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
+# NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
 # PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL LOS ALAMOS NATIONAL
 # SECURITY, LLC OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
 # SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
-# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
-# IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
