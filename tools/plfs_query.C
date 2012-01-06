da9e7da3ee70d66a571e2def9309c18d093c3adc
tj-machado <tj-machado@979fb63a-54b4-4514-8d67-9ab8ea1b59df>
2012-01-06 16:34:26 +0000

    eliminated -logical and -physical, decision is made automatically
    git-svn-id: https://plfs.svn.sourceforge.net/svnroot/plfs/trunk@785 979fb63a-54b4-4514-8d67-9ab8ea1b59df

--- tools/plfs_query.C
+++ tools/plfs_query.C
@@ -4,6 +4,11 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
 using namespace std;
 
 #include "plfs.h"
@@ -13,7 +18,7 @@ using namespace std;
 void 
 show_usage(char* app_name) {
     fprintf(stderr, "Usage: %s -physical <path of PLFS file> [-l] |"
-            " -logical <path of dropping on backend>", app_name);
+            " -logical <path of dropping on backend>\n", app_name);
 }
 
 void
@@ -26,26 +31,23 @@ print_entries(const vector<string> &entries, const char *type) {
 
 //Print the PLFS file from a given backend dropping
 int
-logical_from_physical(char * physical_target) {
+logical_from_physical(char * physical_target, std::string &file_location) {
     char * c_physical;
     string full_physical;
-    string logical_file;
 
     //We'll need the full path to find the PLFS file
     if ( (c_physical = 
             realpath(physical_target, NULL)) == NULL)
     {
-        printf("Cannot resolve filename %s\n", physical_target);
         return (1);
     }
 
     full_physical = string(c_physical);
+    free(c_physical);
     //The name of the PLFS file should be the directory located just
     // above the last hostdir.<number> directory.
     size_t hostdirPos = full_physical.rfind("/hostdir.");
     if (hostdirPos == string::npos) {
-        printf("%s does not appear to belong to a regular plfs file.\n",
-                full_physical.c_str());
         return(1);
     }
 
@@ -56,7 +58,6 @@ logical_from_physical(char * physical_target) {
     //backend belongs to.
     PlfsConf * pconf = get_plfs_conf();
     if (pconf == NULL) {
-        printf("Could not find PlfsConf.\n");
         return(1);
     } else {
         string backend;
@@ -97,95 +98,38 @@ logical_from_physical(char * physical_target) {
                         foundMountPoint = true;
                     }
                 }
-                logical_file = 
+                file_location = 
                     full_physical.replace(0, backend.size(), mountPoint);
             }
         }
 
         if (!foundBackend || !foundMountPoint) {
-            printf("Could not locate logical file location.\n");
             return(1);
         }
 
-        printf("Logical file location is: %s\n", logical_file.c_str());
+        return 0;
     }
-    return 0;
-}
-
-//Prints the physical files from a given PLFS file
-int
-physical_from_logical(char * logical_target,
-        string dir_suffix,
-        string metalink_suffix)
-{
-    vector<string> files;
-    vector<string> dirs;
-    vector<string> metalinks;
-    int ret = plfs_locate(logical_target,
-            (void*)&files,
-            (void*)&dirs,
-            (void*)&metalinks);
-    if ( ret != 0 ) {
-        fprintf(stderr, "Couldn't query %s: %s\n",
-                logical_target, strerror(-ret));
-    } else {
-        print_entries(dirs,dir_suffix.c_str());
-        print_entries(metalinks,metalink_suffix.c_str());
-        print_entries(files,"");
-    }
-    return (ret);
 }
 
 int 
 main (int argc, char **argv) {
     int i;
-    bool locate_logical_file = false;
-    bool locate_physical_file = false;
-    char * physical_target;
-    char * logical_target;
+    char * target;
     bool found_target = false;
     string dir_suffix = "";
     string metalink_suffix = "";
-    int ret;
 
     for (i = 1; i < argc; i++) {
         if (strcmp(argv[i], "-version") == 0) {
             printf("PLFS library:\n\t%s (SVN %s, Built %s)\n", 
                     plfs_tag(), plfs_version(), plfs_buildtime());
             exit(0);
-        } else if (strcmp(argv[i], "-physical") == 0) {
-            if (locate_logical_file) {
-                printf("Please choose -physical or -logical exclusively.\n");
-                exit(1);
-            } else {
-                locate_physical_file = true;
-            }
-        } else if (strcmp(argv[i], "-logical") == 0) {
-            if (locate_physical_file) {
-                printf("Please choose -physical or -logical exclusively.\n");
-                exit(1);
-            } else {
-                locate_logical_file = true;
-            }
         } else if (strcmp(argv[i], "-l") == 0) {
-            if (!locate_logical_file) {
                 dir_suffix = "/";
                 metalink_suffix = "@";
-            } else {
-                printf("-l option not compatible with -logical option."
-                        " Ignoring. \n");
-            }
         } else if (!found_target) {
-            if (locate_physical_file) {
-                logical_target = argv[i];
+                target = argv[i];
                 found_target = true;
-            } else if (locate_logical_file) {
-                physical_target = argv[i];
-                found_target = true;
-            } else {
-                show_usage(argv[0]);
-                exit(1);
-            }
         } else {
             // Found more than one target. This is an error.
             show_usage(argv[0]);
@@ -198,19 +142,43 @@ main (int argc, char **argv) {
         exit(1);
     }
 
-    // we run in two modes
-    // 1) find the logical path to a PLFS file from physical path of a dropping
-    // 2) find the physical path of the top-level container from path of logical
+    vector<string> files;
+    vector<string> dirs;
+    vector<string> metalinks;
+    int oldStrm;
+    int redirectStrm;
 
-    if (locate_logical_file) {
-        ret = logical_from_physical(physical_target);
-    } else if (locate_physical_file) {
-        ret = physical_from_logical(logical_target,dir_suffix,metalink_suffix);
+    //Temporarily redirect stderr to avoid messages from plfs_locate
+    fflush(stderr);
+    oldStrm = dup(2);
+    redirectStrm = ::open("/dev/null", O_WRONLY);
+    ::dup2(redirectStrm, 2);
+
+    //Use the plfs_locate fucntion to determine if this is a
+    //plfs file.
+    int ret = plfs_locate(target,
+            (void*)&files,
+            (void*)&dirs,
+            (void*)&metalinks);
+    
+    //Restore stderr
+    fflush(stderr);
+    ::dup2(oldStrm, 2);
+    ::close(oldStrm);
+    if ( ret != 0 ) {
+        //Not a plfs file, attempt to treat it like a physical file
+        std::string logical_file;
+        if (logical_from_physical(target, logical_file) != 0) {
+            fprintf(stderr, "Couldn't query %s\n", target);
+        } else {
+            printf("Logical file location:\n%s\n", logical_file.c_str());
+        }
     } else {
-        show_usage(argv[0]);
-        exit(1);
+        printf("Physical file locations:\n");
+        print_entries(dirs,dir_suffix.c_str());
+        print_entries(metalinks,metalink_suffix.c_str());
+        print_entries(files,"");
     }
-
 
     exit( ret );
 }
