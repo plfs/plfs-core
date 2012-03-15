7839f775dc8bd0b34b7d76c774079aa277bf82cd
agtorre <agtorre@979fb63a-54b4-4514-8d67-9ab8ea1b59df>
2012-03-15 23:05:54 +0000

    fixed a bug in ad_plfs_close on trunk
    git-svn-id: https://plfs.svn.sourceforge.net/svnroot/plfs/trunk@973 979fb63a-54b4-4514-8d67-9ab8ea1b59df

--- ad_plfs/ad_plfs_close.c
+++ ad_plfs/ad_plfs_close.c
@@ -27,9 +27,9 @@ void ADIOI_PLFS_Close(ADIO_File fd, int *error_code)
     Plfs_close_opt close_opt;
     close_opt.pinter=PLFS_MPIIO;
     int flatten=0;
+    plfs_debug("%s: begin\n", myname );
     MPI_Comm_rank( fd->comm, &rank );
     MPI_Comm_size( fd->comm, &procs);
-    plfs_debug("%s: begin\n", myname );
     close_opt.num_procs = procs;
     amode = ad_plfs_amode( fd->access_mode );
     if(fd->fs_ptr==NULL) {
@@ -40,26 +40,30 @@ void ADIOI_PLFS_Close(ADIO_File fd, int *error_code)
         *error_code = MPI_SUCCESS;
         return;
     }
-    flatten = ad_plfs_hints (fd , rank, "plfs_flatten_close");
-    // Only want to do this on write
     double start_time,end_time;
     start_time=MPI_Wtime();
-    if(flatten && fd->access_mode!=ADIO_RDONLY) {
-        // flatten on close takes care of calling plfs_close and setting
-        // up the close_opt
-        close_opt.valid_meta=0;
-        plfs_debug("Rank: %d in flatten then close\n",rank);
-        err = flatten_then_close(fd, fd->fs_ptr, rank, amode, procs, &close_opt,
-                                 fd->filename,uid);
-    } else {
-        // for ADIO, just 0 creates the openhosts and the meta dropping
-        // Grab the last offset and total bytes from all ranks and reduce to max
-        plfs_debug("Rank: %d in regular close\n",rank);
-        if(fd->access_mode!=ADIO_RDONLY) {
-            reduce_meta(fd, fd->fs_ptr, fd->filename, &close_opt, rank);
+    if (plfs_get_filetype(fd->filename) != CONTAINER) {
+        err = plfs_close(fd->fs_ptr, rank, uid,amode,NULL);
+    }else{
+        flatten = ad_plfs_hints (fd , rank, "plfs_flatten_close");
+        if(flatten && fd->access_mode!=ADIO_RDONLY) {
+            // flatten on close takes care of calling plfs_close and setting
+            // up the close_opt
+            close_opt.valid_meta=0;
+            plfs_debug("Rank: %d in flatten then close\n",rank);
+            err = flatten_then_close(fd, fd->fs_ptr, rank, amode, procs, &close_opt,
+                                     fd->filename,uid);
+        } else {
+            // for ADIO, just 0 creates the openhosts and the meta dropping
+            // Grab the last offset and total bytes from all ranks and reduce to max
+            plfs_debug("Rank: %d in regular close\n",rank);
+            if(fd->access_mode!=ADIO_RDONLY) {
+                reduce_meta(fd, fd->fs_ptr, fd->filename, &close_opt, rank);
+            }
+            err = plfs_close(fd->fs_ptr, rank, uid,amode,&close_opt);
         }
-        err = plfs_close(fd->fs_ptr, rank, uid,amode,&close_opt);
-    }
+    } 
+
     plfs_debug("%d: close time: %.2f\n", rank,MPI_Wtime()-start_time);
     fd->fs_ptr = NULL;
     if (err < 0 ) {
