227b3d31f0c8bf2d97f9de46986cf137195dab9a
agtorre <agtorre@979fb63a-54b4-4514-8d67-9ab8ea1b59df>
2012-05-14 22:32:18 +0000

    re-enabled the lazy_stat and fixed the function to no longer require MPI_Bcast
    git-svn-id: https://plfs.svn.sourceforge.net/svnroot/plfs/trunk@1044 979fb63a-54b4-4514-8d67-9ab8ea1b59df

--- ad_plfs/ad_plfs_fcntl.c
+++ ad_plfs/ad_plfs_fcntl.c
@@ -17,8 +17,6 @@ void ADIOI_PLFS_Fcntl(ADIO_File fd, int flag, ADIO_Fcntl_t *fcntl_struct,
     switch(flag) {
     case ADIO_FCNTL_GET_FSIZE:
         plfs_query(fd->fs_ptr, NULL, NULL, NULL, &lazy_stat);
-        //force lazy_stat to be 0 since the lazy call relies on MPI_Allreduce
-        lazy_stat = 0;
         if (lazy_stat == 0) {
             // every rank calls plfs_sync to flush in-memory index.
             // this is not a collective operation, take out
@@ -46,10 +44,14 @@ void ADIOI_PLFS_Fcntl(ADIO_File fd, int flag, ADIO_Fcntl_t *fcntl_struct,
             size_only = 1;  // do lazy stat
             ret = plfs_getattr( fd->fs_ptr, fd->filename, &buf, size_only );
             if ( ret == 0 ) {
-                long long tmp_buf;
-                MPI_Allreduce(&(buf.st_size), &tmp_buf, 1,
-                              MPI_LONG_LONG, MPI_MAX, fd->comm);
-                fcntl_struct->fsize = (size_t)tmp_buf;
+                //This is a non collective version of the commented
+                //code below
+                fcntl_struct->fsize = buf.st_size;
+                //These all depend on a collective call
+                //long long tmp_buf;
+                //MPI_Allreduce(&(buf.st_size), &tmp_buf, 1,
+                //              MPI_LONG_LONG, MPI_MAX, fd->comm);
+                //fcntl_struct->fsize = (size_t)tmp_buf;
                 *error_code = MPI_SUCCESS;
             } else {
                 *error_code = MPIO_Err_create_code(MPI_SUCCESS,
