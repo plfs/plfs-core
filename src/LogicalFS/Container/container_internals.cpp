a1be61c5e6d8079ca0cd7a8b2e2833b47afbb22f
David Bonnie <dbonnie@lanl.gov>
2013-07-16 10:33:55 -0600

    Added more checks (-Wextra), fixed compiler warning

--- src/LogicalFS/Container/container_internals.cpp
+++ src/LogicalFS/Container/container_internals.cpp
@@ -400,7 +400,7 @@ plfs_file_operation(const char *logical, FileOp& op)
             continue;
         }
         ret = op.op(ritr->bpath.c_str(),
-                    is_container?(unsigned char)DT_CONTAINER:DT_DIR,
+                    is_container?(unsigned char)DT_CONTAINER:(unsigned char)DT_DIR,
                     ritr->back->store);
     }
     if (is_container) {
