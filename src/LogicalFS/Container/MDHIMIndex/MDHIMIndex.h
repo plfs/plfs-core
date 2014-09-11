b8c8e6686335b5d6557250189228a876d4fb44e2
Chuck Cranor <chuck@ece.cmu.edu>
2014-09-11 14:01:06 -0400

    update to match current ContainerIndex.h

--- src/LogicalFS/Container/MDHIMIndex/MDHIMIndex.h
+++ src/LogicalFS/Container/MDHIMIndex/MDHIMIndex.h
@@ -1,3 +1,7 @@
+/*
+ * MDHIMIndex.h  all structures for the MDHIM index
+ */
+
 /**
  * MDHIMIndex: MDHIM instance of PLFS container index
  */
@@ -6,21 +10,37 @@ public:
     MDHIMIndex();    /* constructor */
     ~MDHIMIndex();   /* destructor */
 
-    plfs_error_t index_open(Container_OpenFile *cof, int open_flags);
-    plfs_errot_t index_close(Container_OpenFile *cof, int open_flags);
+    const char *index_name(void) { return("MDHIM"); };
+
+    plfs_error_t index_open(Container_OpenFile *cof, int open_flags,
+                            Plfs_open_opt *oopt);
+    plfs_error_t index_close(Container_OpenFile *cof, off_t *lastoffp,
+                             size_t *tbytesp, Plfs_close_opt *copt);
     plfs_error_t index_add(Container_OpenFile *cof, size_t nbytes,
-                           off_t offset, pid_t pid);
+                           off_t offset, pid_t pid, off_t physoffset,
+                           double begin, double end); 
     plfs_error_t index_sync(Container_OpenFile *cof);
     plfs_error_t index_query(Container_OpenFile *cof, off_t input_offset,
-                             size_t input_length, 
-                             vector<index_record> &result);
-    plfs_error_t index_truncate(struct plfs_physpathinfo *ppip, off_t offset);
-    plfs_error_t index_new_wdrop(Container_OpenFile *cof,
-                                 string ts, pid_t pid);
-    plfs_error_t index_getattr_size(struct plfs_physpathinfo *ppip,
-                                    struct stat *stbuf,
-                                    set<string> *openset,
-                                    set<string> *metaset);
+                             size_t input_length, list<index_record> &result);
+    plfs_error_t index_truncate(Container_OpenFile *cof, off_t offset);
+    plfs_error_t index_closing_wdrop(Container_OpenFile *cof, string ts,
+                                     pid_t pid, const char *filename);
+    plfs_error_t index_new_wdrop(Container_OpenFile *cof, string ts,
+                                 pid_t pid, const char *filename);
+    plfs_error_t index_optimize(Container_OpenFile *cof);
+    plfs_error_t index_info(off_t &lastoff, off_t &bwritten);
+
+    plfs_error_t index_droppings_getattrsize(struct plfs_physpathinfo *ppip,
+                                             struct stat *stbuf,
+                                             set<string> *openset,
+                                             set<string> *metaset);
+
+    plfs_error_t index_droppings_rename(struct plfs_physpathinfo *src,
+                                        struct plfs_physpathinfo *dst);
+    plfs_error_t index_droppings_trunc(struct plfs_physpathinfo *ppip,
+                                               off_t offset);
+    plfs_error_t index_droppings_unlink(struct plfs_physpathinfo *ppip);
+    plfs_error_t index_droppings_zero(struct plfs_physpathinfo *ppip);
 
  private:
     mdhim_t *mdhix;   /* handle to any open mdhim index */
